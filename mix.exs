e3b37662375c7331020ba8e9b34711c4f6841ef2
Josh Adams <jadams@gridpoint.com>
2026-06-04 08:25:38 -0400

    Add Hex package metadata

--- mix.exs
+++ mix.exs
@@ -1,13 +1,17 @@
 defmodule ReqStructureResponse.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/aeturnumfluctus/req_structure_response"
+
   def project do
     [
       app: :req_structure_response,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -22,6 +26,15 @@ defmodule ReqStructureResponse.MixProject do
       {:req, "~> 0.5.0"},
       {:plug, "~> 1.0", only: :test},
       {:ex_doc, ">= 0.0.0", only: :docs}
+    ]
+  end
+
+  defp package do
+    [
+      description: "Req plugin for applying structure to response bodies.",
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @source_url},
+      maintainers: ["Josh Adams"]
     ]
   end
 end
